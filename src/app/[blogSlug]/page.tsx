ed9c0b179db643520ce3559018065e99567bf1e9
Alharith <alhaymex@gmail.com>
2024-12-01 22:51:31 +0300

    Adding 404 to not found blogs

--- src/app/[blogSlug]/page.tsx
+++ src/app/[blogSlug]/page.tsx
@@ -1,4 +1,5 @@
 import { getBlog } from "@/actions/blogs-actions";
+import NotFound from "../not-found";
 
 export default async function BlogPage({
   params,
@@ -11,18 +12,7 @@ export default async function BlogPage({
   const blogResult = await getBlog(blogSlug);
 
   if (!blogResult.success) {
-    console.log("Blog not found for slug:", blogSlug);
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-4xl font-bold mb-4">Blog Not Found</h1>
-        <p className="text-gray-600 mb-8">
-          {`The blog you're looking for doesn't exist.`}
-        </p>
-        <p className="px-4 py-2 bg-primary text-primary-foreground rounded-md hover:bg-primary/90">
-          Return Home
-        </p>
-      </div>
-    );
+    return <NotFound />;
   }
 
   const blog = blogResult.data;
