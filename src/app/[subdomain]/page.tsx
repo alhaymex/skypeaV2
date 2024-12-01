667df0effb1a15a9c8889d99e5d649a818e9c751
Alharith <alhaymex@gmail.com>
2024-12-01 21:25:41 +0300

    Adding dynamic content to the subdomain page

--- src/app/[subdomain]/page.tsx
+++ src/app/[subdomain]/page.tsx
@@ -1,13 +1,28 @@
-"use client";
-import { useRouter } from "next/router";
+import { getBlogBySlug } from "@/actions/blogs-actions";
 
-export default function Page() {
-  const router = useRouter();
-  const { subdomain } = router.query;
+export default async function BlogPage({
+  params,
+}: {
+  params: { blogSlug: string };
+}) {
+  const { blogSlug } = params;
+  console.log("Accessed blogSlug:", blogSlug); // Add this line
+
+  const blogResult = await getBlogBySlug(blogSlug);
+
+  if (!blogResult.success) {
+    console.log("Blog not found for slug:", blogSlug); // Add this line
+    return <div>Blog not found</div>;
+  }
+
+  const blog = blogResult.data;
+  console.log("Rendering blog:", blog!.name); // Add this line
 
   return (
     <div>
-      <h1>Welcome to {subdomain} subdomain!</h1>
+      <h1>{blog!.name}</h1>
+      <p>{blog!.description}</p>
+      {/* Add more blog content here */}
     </div>
   );
 }
