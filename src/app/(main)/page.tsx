2f6155ca1c7b46dd01f51ac3f833d0bd2697e1cd
Alharith <alhaymex@gmail.com>
2024-12-02 21:02:20 +0300

    Making the footer template a client component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,15 +1,12 @@
 import Features from "@/components/pages/home/Features";
 import Hero from "@/components/pages/home/Hero";
 import PricingSection from "@/components/pages/home/Pricing";
-// import Testimonials from "@/components/pages/home/Testimonials";
-import React from "react";
 
 const HomePage = () => {
   return (
     <main>
       <Hero />
       <Features />
-      {/* <Testimonials /> */}
       <PricingSection />
     </main>
   );
