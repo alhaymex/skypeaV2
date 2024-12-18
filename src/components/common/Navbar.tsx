f44c8f6b1cc7fffcbd418cc6e76d94fbd59aa324
Alharith <alhaymex@gmail.com>
2024-12-18 08:34:44 +0300

    Adding the missing pages from the navigations

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -83,24 +83,9 @@ export default function LandingPageNav() {
         <NavigationMenuTrigger>Resources</NavigationMenuTrigger>
         <NavigationMenuContent>
           <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-            <li className="row-span-3">
-              <NavigationMenuLink asChild>
-                <a
-                  className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                  href="/templates"
-                >
-                  <Pen className="h-6 w-6" />
-                  <div className="mb-2 mt-4 text-lg font-medium">Templates</div>
-                  <p className="text-sm leading-tight text-muted-foreground">
-                    Explore our collection of customizable blog and newsletter
-                    templates.
-                  </p>
-                </a>
-              </NavigationMenuLink>
-            </li>
             <li>
               <NavigationMenuLink asChild>
-                <a
+                <Link
                   className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
                   href="/blog"
                 >
@@ -109,12 +94,12 @@ export default function LandingPageNav() {
                     Read our latest articles on content creation and audience
                     engagement.
                   </p>
-                </a>
+                </Link>
               </NavigationMenuLink>
             </li>
             <li>
               <NavigationMenuLink asChild>
-                <a
+                <Link
                   className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
                   href="/about"
                 >
@@ -124,7 +109,7 @@ export default function LandingPageNav() {
                   <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
                     {`Learn more about Skypea's mission and team.`}
                   </p>
-                </a>
+                </Link>
               </NavigationMenuLink>
             </li>
           </ul>
@@ -197,9 +182,6 @@ export default function LandingPageNav() {
                   onClick={(e) => handleNavigation(e, "#pricing")}
                 >
                   Pricing
-                </Link>
-                <Link href="/templates" className="text-lg font-medium">
-                  Templates
                 </Link>
                 <Link href="/blog" className="text-lg font-medium">
                   Blog
