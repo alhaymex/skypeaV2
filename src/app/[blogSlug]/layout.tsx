6401a2dd1b09006616f8e1622a8a839e6b76f6c5
Alharith <alhaymex@gmail.com>
2024-12-20 08:29:10 +0300

    Refactor Blog Post Layout: Moved getValidImageUrl function to utils for better code organization and reduced complexity in layout.tsx

---
+++ src/app/[blogSlug]/layout.tsx
@@ -0,0 +1,49 @@
+import { getBlog } from "@/actions/blogs-actions";
+import { getValidImageUrl } from "@/utils/view-blog";
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { blogSlug: string };
+}) => {
+  const blogSlug = params.blogSlug;
+  const blog = await getBlog(blogSlug);
+
+  const postImage = getValidImageUrl(blog.data?.openGraph);
+
+  return {
+    title: blog.data?.name,
+    description: blog.data?.description,
+    openGraph: {
+      title: blog.data?.name,
+      description: blog.data?.description,
+      type: "article",
+      publishedTime: blog.data?.createdAt?.toISOString(),
+      modifiedTime: blog.data?.updatedAt?.toISOString(),
+      url: `https://${blogSlug}.skypea.net`,
+      images: [
+        {
+          url: postImage,
+          width: 1200,
+          height: 630,
+          alt: blog.data?.name,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: blog.data?.name,
+      description: blog.data?.description || "A blog post on Skypea",
+      images: [postImage],
+    },
+    alternates: {
+      canonical: `https://${blogSlug}.skypea.net`,
+    },
+  };
+};
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return children;
+};
+
+export default layout;
